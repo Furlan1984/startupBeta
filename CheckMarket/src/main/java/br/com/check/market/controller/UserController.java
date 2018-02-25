c31ff68a4fad604546c196c8bc24a5dfa5bbad76
Mayquel Alves <mayquellalves@gmail.com>
2018-02-25 13:11:42 -0300

    Finish CRUD User

--- CheckMarket/src/main/java/br/com/check/market/controller/UserController.java
+++ CheckMarket/src/main/java/br/com/check/market/controller/UserController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +25,7 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 
-	@PostMapping("/new")
+	@PostMapping("/users/new")
 	public User create(@Valid @RequestBody User user) {
 		return userRepository.save(user);
 	}
@@ -29,6 +33,39 @@ public class UserController {
 	@GetMapping("/users")
 	public List<User> getAllUsers() {
 		return userRepository.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getNoteById(@PathVariable(value = "id") Long userId) {
+		User user = userRepository.findOne(userId);
+		if (user == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().body(user);
+	}
+
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<User> deleteNote(@PathVariable(value = "id") Long userId) {
+		User user = userRepository.findOne(userId);
+		if (user == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		userRepository.delete(user);
+		return ResponseEntity.ok().build();
+	}
+
+	@PutMapping("/users/{id}")
+	public ResponseEntity<User> updateNote(@PathVariable(value = "id") Long noteId,
+			@Valid @RequestBody User userDetails) {
+		User user = userRepository.findOne(noteId);
+		if (user == null) {
+			return ResponseEntity.notFound().build();
+		}
+		user.setPassword(userDetails.getPassword());
+
+		User updatedUser = userRepository.save(user);
+		return ResponseEntity.ok(updatedUser);
 	}
 
 }
