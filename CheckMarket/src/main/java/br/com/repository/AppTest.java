eab256c2bcf799e427f8080dd0e6cd06c8123740
Nelson Rodrigo Furlan <nelson.furlan@pwc.com>
2018-08-15 16:55:47 -0300

    hibernate test

---
+++ CheckMarket/src/main/java/br/com/repository/AppTest.java
@@ -0,0 +1,25 @@
+package br.com.repository;
+
+import junit.framework.TestCase;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import br.com.model.AppUser;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+
+	public void testApp() {
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		AppUser user = new AppUser("firstuser");
+		session.save(user);
+		session.getTransaction().commit();
+		session.close();
+	}
+}
---
+++ CheckMarket/src/main/java/br/com/util/HibernateUtilTest.java
@@ -0,0 +1,9 @@
+package br.com.util;
+
+public class HibernateUtilTest {  
+
+public static void main(String args[]) {  
+HibernateUtil.getFactory().openSession();  
+}  
+
+}  
